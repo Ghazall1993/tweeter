0d30b744ad5e64466d8aa7f88c2fcb1438c109c9
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-18 04:51:01 +0000

    create one tweet element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,61 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+let date_diff_indays = function (date1, date2) {
+  dt1 = new Date(date1);
+  dt2 = new Date(date2);
+  return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24));
+}
 
+const createTweetElement = function (tweet) {
+  const $tweet =
+    `
+  <article class="tweet">
+      <header>
+        <div class="left">
+          <img class="profile-picture" src="${tweet.user.avatars}" alt="Profile Picture">
+          <div>${tweet.user.name}</div>
+        </div>
+        <span class="handle">
+          ${tweet.user.handle}
+        </span>
+      </header>
+      <div class="content">
+        ${tweet.content.text}
+      </div>
+      <footer>
+        <div>
+          ${date_diff_indays(tweet.created_at, Date.now())}
+        </div>
+
+        <div>
+          <i class="fa fa-flag"></i>
+          <i class="fa fa-retweet"></i>
+          <i class="fa fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    `;
+  return $tweet;
+};
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://image.flaticon.com/icons/svg/145/145852.svg",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+
+$(document).ready(() => {
+$('.container').append($tweet);
+});
