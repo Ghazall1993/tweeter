cac654a0f6371af018cf2cc919cc11e321a1baf6
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-18 04:59:17 +0000

    implement tweets rendering dynamically and remove the tweet article in index.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,7 +27,7 @@ const createTweetElement = function (tweet) {
       </div>
       <footer>
         <div>
-          ${date_diff_indays(tweet.created_at, Date.now())}
+          ${date_diff_indays(tweet.created_at, Date.now())} days ago
         </div>
 
         <div>
@@ -41,23 +41,39 @@ const createTweetElement = function (tweet) {
   return $tweet;
 };
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://image.flaticon.com/icons/svg/145/145852.svg",
-    "handle": "@SirIsaac"
+const renderTweets = function(tweets) {
+  for (tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $(document).ready(() => {
+      $('.container').append($tweet);
+      });
+  }
+}
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-$(document).ready(() => {
-$('.container').append($tweet);
-});
+renderTweets(data);
