6b57257e8000d1f81429ab1ea3eda6ab2c5c3bb3
Ghazal <ghazal.fouladfard@gmail.com>
2020-06-18 01:36:15 +0000

    update hover and fix the navbar

--- public/scripts/tweet-hover.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("article.tweet").hover(function () {
-    console.log("found")
-    $(this).css("box-shadow", "10px 5px 5px #98AFC7");
-  }, function () {
-    $(this).css("box-shadow", "none");
-  });
-});
